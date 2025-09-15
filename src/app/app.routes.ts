95daee17244a49ea882df29f29cf3c67c2007202
Brian Vega <bvega@griddynamics.com>
2025-09-15 16:30:37 -0600

    feat(bday-page): Add component in routes and add Provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'birthday-countdown/:day/:month/:name', 
-        loadComponent: () => import('./birthday-countdown/birthday-countdown.component').then(c => c.BirthdayCountdownComponent) 
-    }
+      path: 'birthday-countdown/:day/:month/:name',
+      loadComponent: () => import('./birthday-countdown/birthday-countdown.component').then(c => c.BirthdayCountdownComponent)
+    },
+    {
+      path: 'happy-birthday',
+      loadComponent: () => import('./birthday-landing-page/birthday-landing-page.component').then(c => c.BirthdayLandingPageComponent)
+    },
 ];
